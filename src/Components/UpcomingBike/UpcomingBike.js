89df8a86acecba5df462e3781c3dcd6bf57b9e24
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-04-30 08:04:34 +0600

    upcoming bike section modified

--- src/Components/UpcomingBike/UpcomingBike.js
+++ src/Components/UpcomingBike/UpcomingBike.js
@@ -8,8 +8,8 @@ const UpcomingBike = () => {
             <div className='upcoming-bike w-75 mx-auto'>
             <h1 className='mt-5'>Upcoming Bikes</h1>
             <h3 className='mt-3'>Yamaha R15 v4</h3>
-            <p>Bikers Headquarter LTD the sole distributor of Yamaha motorcycles in Bangladesh did a great job for Yamaha this year. Yamaha planning to launch some exciting products in the upcoming year and their wild card will be Yamaha R15 v4. Although there has been no massive change in the new R15 v4, but it’s design more sharp. now it’s look like Yamaha R6. however some advanced feature added in this new version. Such as Charging port, Bluetooth mobile connectivity, riding modes and many more. BIkers Headquarter LTD will officially launched this bike in Bangladeshi market probably in January or February.</p>
-            <img src={bike} alt="Img"/>
+            <p>Yamaha planning to launch some exciting products in the upcoming year and their wild card will be Yamaha R15 v4. Although there has been no massive change in the new R15 v4, but it’s design more sharp. now it’s look like Yamaha R6. however some advanced feature added in this new version. BIkers Headquarter LTD will officially launched this bike in Bangladeshi market probably in January or February.</p>
+            <img className='img-fluid' src={bike} alt="Img"/>
             <figcaption>Yamaha R15 v4 Official Photo</figcaption>
         </div>
         </div>
