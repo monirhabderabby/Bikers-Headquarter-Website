1bb50f10ee5a66e29911756ae997425dff2b70d2
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-06 09:23:07 +0600

    add product to mongodb successfully

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import google from '../Assets/logo/google.svg'
 import './Login.css'
 import { Link, useLocation, useNavigate } from 'react-router-dom';
--- src/Components/MyPage/MyPage.js
+++ src/Components/MyPage/MyPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState} from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/firebase.init';
 import Navbar from '../Shared/Navbar/Navbar';
@@ -6,32 +7,79 @@ import './MyPage.css'
 
 const MyPage = () => {
     const [user] = useAuthState(auth);
+
+    const [productName, setProductName] = useState('');
+    const [productPrice, setProductPrice] =  useState(0);
+    const [imgLink, setImgLink] = useState("");
+    const [quantity, setQuantity] = useState(0);
+    const [supplier, setSupplier] = useState("");
+    const [productDescription, setProductDescription] = useState("");
+
+
+
+    const handleProductName = e =>{
+        setProductName(e.target.value)
+    }
+    const handleProductPrice = e =>{
+        setProductPrice(e.target.value)
+    }
+    const handleImgLink = e =>{
+        setImgLink(e.target.value)
+    }
+    const handleQuantity = e =>{
+        setQuantity(e.target.value)
+    }
+    const handleSupplier = e =>{
+        setSupplier(e.target.value)
+    }
+    const handleDescription = e =>{
+        setProductDescription(e.target.value);
+    }
+    const addProduct = async e =>{
+        e.preventDefault();
+        const email = user.email;
+        const product = ({email, productName, productPrice, imgLink, quantity, supplier, productDescription});
+        fetch('http://localhost:5000/addProduct', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(product)
+        })
+        .then(res=> res.json())
+        .then(data=> {
+            if(data.acknowledged === true){
+                e.target.reset();
+            }
+        })
+
+    }
     return (
         <div>
             <Navbar></Navbar>
             <div className='container w-100 d-flex justify-content-center mt-4'>
-                <form className='product-form'>
+                <form className='product-form' onSubmit={addProduct}>
                     <h2>Listing a new product!</h2>
                 <div className='input-field'>
                         <input  type="email" name="email" value={user.email} readOnly required/>
                 </div>
                 <div className='input-field'>
-                    <input  type="text" name="Product-name" placeholder='Product name' required/>
+                    <input  type="text" name="ProductName" onBlur={handleProductName} placeholder='Product name' required/>
                 </div>
                 <div className='input-field'>
-                    <input  type="text" name="price" placeholder='Product price' required/>
+                    <input  type="text" name="price" onBlur={handleProductPrice} placeholder='Product price' required/>
                 </div>
                 <div className='input-field'>
-                    <input  type="text" name="image" placeholder='Product image link' required/>
+                    <input  type="text" name="image" onBlur={handleImgLink}  placeholder='Product image link' required/>
                 </div>
                 <div className='input-field'>
-                    <input type="number" name="quantity" placeholder='Quantity' required/>
+                    <input type="number" name="quantity" onBlur={handleQuantity} placeholder='Quantity' required/>
                 </div>
                 <div className='input-field'>
-                    <input type="text" name="supplier" placeholder='Supplier name' required/>
+                <input  type="text" name="email" onBlur={handleSupplier} placeholder="supplier type only Yamaha/Suzuki/Ktm/Honda" required/>
                 </div>
                 <div className='input-field'>
-                    <textarea name="description" cols="30" rows="3" placeholder='Short description'></textarea>
+                    <textarea name="description" onBlur={handleDescription} cols="30" rows="3" placeholder='Short description'></textarea>
                 </div>
                 <div className='input-field'>
                     <input type="submit" value="Add Product" />
